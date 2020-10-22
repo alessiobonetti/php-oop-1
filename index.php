5146a2ad0b6a48cdaecac0ac2b18689a5100005d
Alessio Bonetti <alesine87@gmail.com>
2020-10-22 17:34:57 +0200

    users dentro div

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ class User {
     public $eta;
     public $email;
     public $password;
-    public $username
+    public $username;
 
     public function __construct($_email, $_password, $_username){
         $this->email = $_email;
@@ -16,3 +16,43 @@ class User {
 }
 
 $user1 = new User ("alesine87@gmail.com" , "ale87" , "sibert");
+$user2 = new User ("afro@gmail.com", "africboy", "blackmanrule");
+$user3 = new User ("caradelevingne@gmail.com", "caradln145", "delevingne");
+$user4 = new User ("aldo@gmail.com" , "aldorius", "aldiux");
+
+$users = [$user1,$user2,$user3,$user4] ;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Users</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php foreach($users as $user) { ?>
+
+        <div class="user">
+            <ul>
+                <li>
+                    <h2>Username</h2>
+                    <p> <?php echo $user->username ?> </p>
+                </li>
+
+                <li>
+                    <h2>Email</h2>
+                    <p> <?php echo $user->email ?> </p>
+                </li>
+
+                <li>
+                    <h2>Password</h2>
+                    <p> <?php echo $user->password ?> </p>
+                </li>
+            </ul>
+        </div>
+
+    <?php } ?>
+</body>
+</html>
